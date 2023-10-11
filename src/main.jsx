e859c38dc9c046884873ff1a12c0bfc54a4ddef2
balafoutisp <thrylos012@gmail.com>
2023-10-11 16:21:22 +0300

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-import './index.css';
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <ToastContainer position="top-center" autoClose={2000} />
         <App />
